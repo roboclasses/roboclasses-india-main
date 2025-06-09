5345faf42becfb3e0a99e9ffeb2b7f37577ff181
Pritam Nandy <pritamimca1003@gmail.com>
2025-06-09 10:13:31 +0530

    work

--- src/components/homeschooling/AboutUs.tsx
+++ src/components/homeschooling/AboutUs.tsx
@@ -1,3 +1,4 @@
+import { Card } from "../ui/card";
 import { AboutHomeSchooling } from "@/data/AboutData";
 import { DialogDemo } from "@/demo/DialogDemo";
 import Image from "next/image";
@@ -5,41 +6,34 @@ import React from "react";
 
 const AboutUs = () => {
   return (
-    <div className="flex flex-col gap-6 items-center p-20 bg-white dark:bg-black rounded-3xl">
-
-      <div className="flex flex-col text-center gap-4">
-        <p className="font-bold text-4xl">
-          Why study robotics and why with us?
+    <div className="flex flex-col lg:gap-10 gap-8 items-center lg:p-20 p-10">
+      <div className="flex flex-col text-center lg:gap-6 gap-2 items-center">
+        <p className="font-extrabold lg:text-5xl text-2xl bg-clip-text text-transparent bg-custom-gradient lg:p-2">
+          Why study coding and robotics with us?
         </p>
-        <p
-          className="text-sm font-bold text-gray-500"
-          style={{ letterSpacing: "0.05em" }}
-        >
+
+        <p className="lg:text-base text-xs" style={{ letterSpacing: "0.05em" }}>
           Online learning offers a new way to explore?
         </p>
       </div>
-      <div className="flex flex-col flex-wrap gap-6 items-center">
-          <p className="font-extrabold text-lg text-orange-700">
-            Why study with us?
-          </p>
-          
+      <div className="flex flex-col flex-wrap gap-6 items-center" style={{ letterSpacing: "0.05em" }} >
+        <p className="font-bold lg:text-xl text-orange-700">Why study with us?</p>
 
-        <div className="flex flex-row items-center gap-4">
+        <div className="grid lg:grid-cols-3 grid-cols-1 gap-4">
           {AboutHomeSchooling.map((about) => (
-            <div className="flex flex-col gap-2" key={about.title}>
+            <Card className="flex flex-col gap-2 p-4 lg:h-[200px] h-[160px] lg:w-[350px] w-[300px] border-1 relative" key={about.title}>
+              <div className="absolute top-0 right-0 w-40 h-full bg-orange-200 rounded-l-full opacity-10"></div>
               <Image src={about.src} alt={about.alt} height={30} width={30} />
-              <p className="font-bold">{about.title}</p>
-              <div
-                className="text-xs font-semibold text-gray-600"
-                style={{ letterSpacing: "0.05em" }}
-              >
+              <p className="font-bold lg:text-base text-sm">{about.title}</p>
+              <div className="lg:text-sm text-xs text-balance font-medium" style={{letterSpacing: "0.05em"}}>
                 <p>{about.descOne}</p>
                 <p>{about.descTwo}</p>
               </div>
-            </div>
+            </Card>
           ))}
         </div>
-        <div>
+
+        <div className="mt-5">
           <DialogDemo />
         </div>
       </div>
