a8dab302ffb10de08996ed63fbf8a327fb320d13
Pritam Nandy <pritamimca1003@gmail.com>
2025-06-17 15:18:34 +0530

    fix

--- src/components/robotics/LearningPath.tsx
+++ src/components/robotics/LearningPath.tsx
@@ -9,40 +9,46 @@ import React from "react";
 const LearningPath = () => {
   const Items = [
     {
-      src: CAR,
+      id: 1,
       age: "6 - 9",
       description: "Introduction to Robotics and programming",
     },
     {
-      src: BIKE,
+      id: 2,
       age: "9 - 13",
       description: "Advance Robotics and programming",
     },
     {
-      src: VR,
+      id: 3,
       age: "13 - 18",
-      description:
-        "Programming  AR, VR course, Iot and advanced robotics courses",
+      description: "Programming  AR, VR course, Iot and advanced robotics courses",
     },
-    { src: AIML, age: "18+", description: "Machine Learning and AI" },
+    { id: 4, 
+      age: "18+", 
+      description: "Machine Learning and AI"
+    },
   ];
 
   return (
     <div className="flex flex-col lg:gap-10 gap-8 items-center lg:p-20 p-10">
       <div className="flex flex-col lg:gap-6 gap-2 text-center">
-        <h1 className="lg:text-4xl text-2xl font-bold">Child’s learning path</h1>
-        <p className="lg:text-base text-xs" style={{ letterSpacing: "0.05em" }}>Recommended learning path</p>
+        <h1 className="lg:text-4xl text-2xl font-bold">
+          Child’s learning path
+        </h1>
+        <p className="lg:text-base text-xs" style={{ letterSpacing: "0.05em" }}>
+          Recommended learning path
+        </p>
       </div>
       <div className="grid lg:grid-cols-4 grid-cols-2 gap-6 relative">
         {Items.map((item) => (
           <Card
             className="lg:w-[220px] w-[150px] lg:h-[250px] h-[200px] dark:bg-slate-800/40 shadow-md hover:shadow-2xl transition-shadow duration-150 delay-75 
-            ease-linear dark:shadow-black bg-[#F0F0F0] relative"
+            ease-linear dark:shadow-black relative bg-muted"
             key={item.age}
           >
             <CardContent className="flex flex-col items-center gap-4 py-4">
               <Image
-                src={item.src}
+                src={AIML}
                 alt="resource-image"
                 width={146.44}
                 height={90}
@@ -50,15 +56,13 @@ const LearningPath = () => {
               />
               <div className="flex flex-col items-center gap-2 absolute bottom-12">
                 <strong className="font-extrabold">{item.age}</strong>
-                <p className="text-xs text-center">
-                  {item.description}
-                </p>
+                <p className="text-sm text-center">{item.description}</p>
               </div>
             </CardContent>
           </Card>
         ))}
-        <div className='absolute bottom-2 left-20 lg:flex hidden'>
-        <ProgressBarDemo />
+        <div className="absolute bottom-2 left-20 lg:flex hidden">
+          <ProgressBarDemo />
         </div>
       </div>
       <div>
